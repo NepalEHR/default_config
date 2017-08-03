34c645229e4e03e0c3aff75f5f223944a2879684
Sanjay poudel <sanjaypoudel@me.com>
2017-08-03 13:06:00 +0545

    test
    First commit

--- openmrs/apps/registration/fieldValidation.js
+++
@@ -1,8 +0,0 @@
-Bahmni.Registration.customValidator = {
-    "address.address1": {
-        method: function (name, value, personAttributeDetails) {
-            return _.first(_.toString(value)) != 0;
-        },
-        errorMessage: "REGISTRATION_WARD_NUMBER_ERROR_KEY"
-    }
-};
